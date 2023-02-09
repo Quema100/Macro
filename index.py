829f7740cc2be2410ce482ea38dec2735998efc0
Quema <105044971+Quema100@users.noreply.github.com>
2023-02-09 16:37:24 +0900

    Add files via upload

--- index.py
+++ index.py
@@ -4,6 +4,45 @@ import keyboard
 import time 
 import threading
 from tkinter import messagebox
+import pyperclip
+def th3():
+    th = threading.Thread(target=press3)
+    th.daemon = True
+    th.start()
+def press3():
+    global input_text
+    input_text=E.get()
+    global n
+    n=C.get()
+    try:
+      n=float(n)
+      L7.configure(text=f"누르실 키는\"{input_text}\"입니다")
+      time.sleep(1)
+      L7.configure(text="start:\'1\' exit:\'2\' ")  
+      while 1:    
+          if keyboard.is_pressed('1'):       
+              L7.configure(text="stop:\'5\'") 
+              while 1: 
+                    pyperclip.copy(input_text) 
+                    pyautogui.hotkey('ctrl', 'v')
+                    print('Do it')
+                    time.sleep(n) 
+                    if keyboard.is_pressed('5'):
+                     L7.configure(text="Restart:click button")
+                     time.sleep(3)
+                     L7.configure(text="")                                        
+                     print("stop program.")      
+                     break                  
+          if keyboard.is_pressed('2'):
+            L7.configure(text="Restart:click button")
+            time.sleep(3)
+            L7.configure(text="")
+            print("stop program.")      
+            break
+    except:
+      
+      messagebox.showwarning("경고","입력하신 시간이 이상합니다")
+      
 def th2():
     th = threading.Thread(target=press2)
     th.daemon = True
@@ -89,11 +128,11 @@ def press():
         
     
 window1=tk.Tk()
-window1.geometry("600x500")
+window1.geometry("600x600")
 window1.resizable(False,False)
 window1.title('Macro')
 
-L1=tk.Label(window1,text=" 원하는 키를 입력하세요 \n ex) f12,enter,shift....",height=4)
+L1=tk.Label(window1,text="원하는 키를 입력하세요 \n ex) f12,enter,shift....",height=4)
 L1.pack()
 
 A=tk.Entry(window1)
@@ -105,8 +144,23 @@ L0.pack()
 B=tk.Button(window1, text="Click",command=th)
 B.pack()
 
-L2=tk.Label(window1, text=' ',height=5)
+L2=tk.Label(window1, text=' ',height=1)
 L2.pack()
+
+L6=tk.Label(window1,text="원하시는 문장 또는 단어를 입력하세요",height=4)
+L6.pack()
+
+E=tk.Entry(window1)
+E.pack(ipadx=10, padx=10)
+
+L8=tk.Label(window1,text="",height=1)
+L8.pack()
+
+B=tk.Button(window1, text="Click",command=th3)
+B.pack()
+
+L7=tk.Label(window1, text=' ',height=2)
+L7.pack()
 
 L3=tk.Label(window1,text="설정할 시간을 입력하세요",height=2)
 L3.pack()
@@ -120,7 +174,7 @@ L4.pack()
 D=tk.Button(window1, text="Click",command=th2)
 D.pack()
 
-L5=tk.Label(window1, text=' ',height=5)
+L5=tk.Label(window1, text=' ',height=2)
 L5.pack()
 
 
@@ -129,5 +183,3 @@ L5.pack()
 
 
 window1.mainloop()
-
-
